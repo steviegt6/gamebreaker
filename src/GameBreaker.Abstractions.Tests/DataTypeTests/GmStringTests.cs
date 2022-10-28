bd5f6de48ea4cd04a6da74715005b98b2fc0ec23
Tomat <xxlennygamerxx@gmail.com>
2022-10-27 20:20:07 -0700

    Fix bug with GmString writing, reorganize unit tests, and add unit tests for GmString.

---
+++ src/GameBreaker.Abstractions.Tests/DataTypeTests/GmStringTests.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Tomat. Licensed under the GPL License, version 2.
+// See the LICENSE file in the repository root for full terms and conditions.
+
+namespace GameBreaker.Abstractions.Tests.DataTypeTests;
+
+public class GmStringTests
+{
+    
+}

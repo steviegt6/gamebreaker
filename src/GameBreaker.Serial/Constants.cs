6fdb7c9e1428d357d068819f609e703030d8da08
Tomat <xxlennygamerxx@gmail.com>
2023-02-21 19:35:14 -0800

    Add constants for remaining chunks.

--- src/GameBreaker.Serial/Constants.cs
+++ src/GameBreaker.Serial/Constants.cs
@@ -8,4 +8,35 @@ public static class Constants {
 
     public const string FORM = "FORM";
     public const string GEN8 = "GEN8";
+    public const string OPTN = "OPTN";
+    public const string LANG = "LANG";
+    public const string EXTN = "EXTN";
+    public const string STRG = "STRG";
+    public const string TPAG = "TPAG";
+    public const string TXTR = "TXTR";
+    public const string AGRP = "AGRP";
+    public const string AUDO = "AUDO";
+    public const string SOND = "SOND";
+    public const string BGND = "BGND";
+    public const string PATH = "PATH";
+    public const string EMBI = "EMBI";
+    public const string DAFL = "DAFL";
+    public const string TGIN = "TGIN";
+    public const string FONT = "FONT";
+    public const string SPRT = "SPRT";
+    public const string ACRV = "ACRV";
+    public const string FUNC = "FUNC";
+    public const string VARI = "VARI";
+    public const string SCPT = "SCPT";
+    public const string TAGS = "TAGS";
+    public const string ROOM = "ROOM";
+    public const string OBJT = "OBJT";
+    public const string TMLN = "TMLN";
+    public const string GLOB = "GLOB";
+    public const string GMEN = "GMEN";
+    public const string SHDR = "SHDR";
+    public const string CODE = "CODE";
+    public const string SEQN = "SEQN";
+    public const string FEDS = "FEDS";
+    public const string FEAT = "FEAT";
 }

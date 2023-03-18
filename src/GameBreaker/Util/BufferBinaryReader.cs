fad46228c3f3d717ac29a04e2204c6fa61b3a7a2
Tomat <xxlennygamerxx@gmail.com>
2023-03-17 20:51:03 -0700

    Quick and dirty changes to BufferBinaryReader.

--- src/GameBreaker/Util/BufferBinaryReader.cs
+++ src/GameBreaker/Util/BufferBinaryReader.cs
@@ -26,27 +26,33 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
+// TODO: Better exceptions.
 namespace GameBreaker.Util
 {
     public class BufferBinaryReader : IPositionable, IEncodable
     {
-        private readonly byte[] buffer;
         public Encoding Encoding { get; private set; }
 
         public int Offset { get; set; }
-        public int Length { get; private set; }
-        public byte[] Buffer { get => buffer; }
 
-        public BufferBinaryReader(Stream stream)
-        {
-            Length = (int)stream.Length;
-            buffer = new byte[Length];
-            Offset = 0;
+        public int Length => Buffer.Length;
 
-            if (stream.Position != 0)
-                stream.Seek(0, SeekOrigin.Begin);
-            stream.Read(buffer, 0, Length);
+        public byte[] Buffer { get; }
+
+        // TODO: Provide ctor capable of using slices? Meh... not useful...?
+        protected BufferBinaryReader(Stream stream) {
+            // TODO: Figure out an acceptable way to handle large files.
+            if (stream.Length > int.MaxValue)
+                throw new IOException("Stream is too large");
+
+            Buffer = new byte[stream.Length];
+            
+            stream.Seek(0, SeekOrigin.Begin);
+            var bytes = stream.Read(Buffer, 0, Length);
             stream.Close();
+            
+            if (bytes != Length)
+                throw new IOException("Stream read failed");
 
             Encoding = new UTF8Encoding(false);
         }
@@ -57,7 +63,7 @@ namespace GameBreaker.Util
             if (Offset < 0 || Offset + 1 > Length)
                 throw new IOException("Reading out of bounds");
 #endif
-            return buffer[Offset++];
+            return Buffer[Offset++];
         }
 
         public virtual bool ReadBoolean()
@@ -73,7 +79,7 @@ namespace GameBreaker.Util
 #endif
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < count; i++)
-                sb.Append(Convert.ToChar(buffer[Offset++]));
+                sb.Append(Convert.ToChar(Buffer[Offset++]));
             return sb.ToString();
         }
 
@@ -94,7 +100,7 @@ namespace GameBreaker.Util
             if (Offset < 0 || Offset + 2 > Length)
                 throw new IOException("Reading out of bounds");
 #endif
-            return (short)(buffer[Offset++] | buffer[Offset++] << 8);
+            return (short)(Buffer[Offset++] | Buffer[Offset++] << 8);
         }
 
         public ushort ReadUInt16()
@@ -103,7 +109,7 @@ namespace GameBreaker.Util
             if (Offset < 0 || Offset + 2 > Length)
                 throw new IOException("Reading out of bounds");
 #endif
-            return (ushort)(buffer[Offset++] | buffer[Offset++] << 8);
+            return (ushort)(Buffer[Offset++] | Buffer[Offset++] << 8);
         }
 
         public int ReadInt24()
@@ -112,7 +118,7 @@ namespace GameBreaker.Util
             if (Offset < 0 || Offset + 3 > Length)
                 throw new IOException("Reading out of bounds");
 #endif
-            return (int)(buffer[Offset++] | buffer[Offset++] << 8 | (sbyte)buffer[Offset++] << 16);
+            return (int)(Buffer[Offset++] | Buffer[Offset++] << 8 | (sbyte)Buffer[Offset++] << 16);
         }
 
         public uint ReadUInt24()
@@ -121,7 +127,7 @@ namespace GameBreaker.Util
             if (Offset < 0 || Offset + 3 > Length)
                 throw new IOException("Reading out of bounds");
 #endif
-            return (uint)(buffer[Offset++] | buffer[Offset++] << 8 | buffer[Offset++] << 16);
+            return (uint)(Buffer[Offset++] | Buffer[Offset++] << 8 | Buffer[Offset++] << 16);
         }
 
         public int ReadInt32()
@@ -130,8 +136,8 @@ namespace GameBreaker.Util
             if (Offset < 0 || Offset + 4 > Length)
                 throw new IOException("Reading out of bounds");
 #endif
-            return (int)(buffer[Offset++] | buffer[Offset++] << 8 | 
-                         buffer[Offset++] << 16 | (sbyte)buffer[Offset++] << 24);
+            return (int)(Buffer[Offset++] | Buffer[Offset++] << 8 | 
+                         Buffer[Offset++] << 16 | (sbyte)Buffer[Offset++] << 24);
         }
 
         public uint ReadUInt32()
@@ -140,8 +146,8 @@ namespace GameBreaker.Util
             if (Offset < 0 || Offset + 4 > Length)
                 throw new IOException("Reading out of bounds");
 #endif
-            return (uint)(buffer[Offset++] | buffer[Offset++] << 8 | 
-                          buffer[Offset++] << 16 | buffer[Offset++] << 24);
+            return (uint)(Buffer[Offset++] | Buffer[Offset++] << 8 | 
+                          Buffer[Offset++] << 16 | Buffer[Offset++] << 24);
         }
 
         public float ReadSingle()
@@ -150,7 +156,7 @@ namespace GameBreaker.Util
             if (Offset < 0 || Offset + 4 > Length)
                 throw new IOException("Reading out of bounds");
 #endif
-            float val = BitConverter.ToSingle(buffer, Offset);
+            float val = BitConverter.ToSingle(Buffer, Offset);
             Offset += 4;
             return val;
         }
@@ -161,7 +167,7 @@ namespace GameBreaker.Util
             if (Offset < 0 || Offset + 8 > Length)
                 throw new IOException("Reading out of bounds");
 #endif
-            double val = BitConverter.ToDouble(buffer, Offset);
+            double val = BitConverter.ToDouble(Buffer, Offset);
             Offset += 8;
             return val;
         }
@@ -172,7 +178,7 @@ namespace GameBreaker.Util
             if (Offset < 0 || Offset + 8 > Length)
                 throw new IOException("Reading out of bounds");
 #endif
-            long val = BitConverter.ToInt64(buffer, Offset);
+            long val = BitConverter.ToInt64(Buffer, Offset);
             Offset += 8;
             return val;
         }
@@ -183,7 +189,7 @@ namespace GameBreaker.Util
             if (Offset < 0 || Offset + 8 > Length)
                 throw new IOException("Reading out of bounds");
 #endif
-            ulong val = BitConverter.ToUInt64(buffer, Offset);
+            ulong val = BitConverter.ToUInt64(Buffer, Offset);
             Offset += 8;
             return val;
         }

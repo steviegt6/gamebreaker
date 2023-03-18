932e6177eaeb6c06c950e0fd875e6e2073d5c86e
Tomat <xxlennygamerxx@gmail.com>
2023-03-18 12:46:54 -0700

    Allow LittleEndianBitConverterTests to work on big endian systems.

--- src/GameBreaker.Tests/Serial/LittleEndianBitConverterTests.cs
+++ src/GameBreaker.Tests/Serial/LittleEndianBitConverterTests.cs
@@ -38,159 +38,213 @@ public static class LittleEndianBitConverterTests {
         0x08,
     };
 
-    [SetUp]
-    public static void SetUp() {
-        if (!BitConverter.IsLittleEndian)
-            throw new Exception("Test only works on little endian systems.");
-    }
-
     [Test]
     public static void TestToInt16() {
         const short expected = 0x0201;
 
-        IsLittleEndian = true;
-        var actualLittle = ToInt16(test_bytes, 0);
-        IsLittleEndian = false;
-        var actualBig = ToInt16(test_bytes, 0);
+        if (BitConverter.IsLittleEndian) {
+            IsLittleEndian = true;
+            var actualLittle = ToInt16(test_bytes, 0);
+            IsLittleEndian = false;
+            var actualBig = ToInt16(test_bytes, 0);
 
-        Assert.Multiple(() => {
-            Assert.That(actualLittle, Is.EqualTo(expected));
+            Assert.Multiple(() => {
+                Assert.That(actualLittle, Is.EqualTo(expected));
+                Assert.That(actualBig, Is.EqualTo(expected));
+            });
+        }
+        else {
+            var actualBig = ToInt16(test_bytes, 0);
             Assert.That(actualBig, Is.EqualTo(expected));
-        });
+        }
     }
 
     [Test]
     public static void TestToUInt16() {
         const ushort expected = 0x0201;
 
-        IsLittleEndian = true;
-        var actualLittle = ToUInt16(test_bytes, 0);
-        IsLittleEndian = false;
-        var actualBig = ToUInt16(test_bytes, 0);
-        
-        Assert.Multiple(() => {
-            Assert.That(actualLittle, Is.EqualTo(expected));
+        if (BitConverter.IsLittleEndian) {
+            IsLittleEndian = true;
+            var actualLittle = ToUInt16(test_bytes, 0);
+            IsLittleEndian = false;
+            var actualBig = ToUInt16(test_bytes, 0);
+
+            Assert.Multiple(() => {
+                Assert.That(actualLittle, Is.EqualTo(expected));
+                Assert.That(actualBig, Is.EqualTo(expected));
+            });
+        }
+        else {
+            var actualBig = ToUInt16(test_bytes, 0);
             Assert.That(actualBig, Is.EqualTo(expected));
-        });
+        }
     }
 
     [Test]
     public static void TestToInt24() {
         const int expected = 0x030201;
 
-        IsLittleEndian = true;
-        var actualLittle = ToInt24(test_bytes, 0);
-        IsLittleEndian = false;
-        var actualBig = ToInt24(test_bytes, 0);
-        
-        Assert.Multiple(() => {
-            Assert.That(actualLittle, Is.EqualTo(new Int24(expected)));
+        if (BitConverter.IsLittleEndian) {
+            IsLittleEndian = true;
+            var actualLittle = ToInt24(test_bytes, 0);
+            IsLittleEndian = false;
+            var actualBig = ToInt24(test_bytes, 0);
+
+            Assert.Multiple(() => {
+                Assert.That(actualLittle, Is.EqualTo(new Int24(expected)));
+                Assert.That(actualBig, Is.EqualTo(new Int24(expected)));
+            });
+        }
+        else {
+            var actualBig = ToInt24(test_bytes, 0);
             Assert.That(actualBig, Is.EqualTo(new Int24(expected)));
-        });
+        }
     }
 
     [Test]
     public static void TestToUInt24() {
         const uint expected = 0x030201;
 
-        IsLittleEndian = true;
-        var actualLittle = ToUInt24(test_bytes, 0);
-        IsLittleEndian = false;
-        var actualBig = ToUInt24(test_bytes, 0);
-        
-        Assert.Multiple(() => {
-            Assert.That(actualLittle, Is.EqualTo(new UInt24(expected)));
+        if (BitConverter.IsLittleEndian) {
+            IsLittleEndian = true;
+            var actualLittle = ToUInt24(test_bytes, 0);
+            IsLittleEndian = false;
+            var actualBig = ToUInt24(test_bytes, 0);
+
+            Assert.Multiple(() => {
+                Assert.That(actualLittle, Is.EqualTo(new UInt24(expected)));
+                Assert.That(actualBig, Is.EqualTo(new UInt24(expected)));
+            });
+        }
+        else {
+            var actualBig = ToUInt24(test_bytes, 0);
             Assert.That(actualBig, Is.EqualTo(new UInt24(expected)));
-        });
+        }
     }
 
     [Test]
     public static void TestToInt32() {
         const int expected = 0x04030201;
 
-        IsLittleEndian = true;
-        var actualLittle = ToInt32(test_bytes, 0);
-        IsLittleEndian = false;
-        var actualBig = ToInt32(test_bytes, 0);
-        
-        Assert.Multiple(() => {
-            Assert.That(actualLittle, Is.EqualTo(expected));
+        if (BitConverter.IsLittleEndian) {
+            IsLittleEndian = true;
+            var actualLittle = ToInt32(test_bytes, 0);
+            IsLittleEndian = false;
+            var actualBig = ToInt32(test_bytes, 0);
+
+            Assert.Multiple(() => {
+                Assert.That(actualLittle, Is.EqualTo(expected));
+                Assert.That(actualBig, Is.EqualTo(expected));
+            });
+        }
+        else {
+            var actualBig = ToInt32(test_bytes, 0);
             Assert.That(actualBig, Is.EqualTo(expected));
-        });
+        }
     }
 
     [Test]
     public static void TestToUInt32() {
         const uint expected = 0x04030201;
 
-        IsLittleEndian = true;
-        var actualLittle = ToUInt32(test_bytes, 0);
-        IsLittleEndian = false;
-        var actualBig = ToUInt32(test_bytes, 0);
-        
-        Assert.Multiple(() => {
-            Assert.That(actualLittle, Is.EqualTo(expected));
+        if (BitConverter.IsLittleEndian) {
+            IsLittleEndian = true;
+            var actualLittle = ToUInt32(test_bytes, 0);
+            IsLittleEndian = false;
+            var actualBig = ToUInt32(test_bytes, 0);
+
+            Assert.Multiple(() => {
+                Assert.That(actualLittle, Is.EqualTo(expected));
+                Assert.That(actualBig, Is.EqualTo(expected));
+            });
+        }
+        else {
+            var actualBig = ToUInt32(test_bytes, 0);
             Assert.That(actualBig, Is.EqualTo(expected));
-        });
+        }
     }
 
     [Test]
     public static void TestToInt64() {
         const long expected = 0x0807060504030201;
 
-        IsLittleEndian = true;
-        var actualLittle = ToInt64(test_bytes, 0);
-        IsLittleEndian = false;
-        var actualBig = ToInt64(test_bytes, 0);
-        
-        Assert.Multiple(() => {
-            Assert.That(actualLittle, Is.EqualTo(expected));
+        if (BitConverter.IsLittleEndian) {
+            IsLittleEndian = true;
+            var actualLittle = ToInt64(test_bytes, 0);
+            IsLittleEndian = false;
+            var actualBig = ToInt64(test_bytes, 0);
+
+            Assert.Multiple(() => {
+                Assert.That(actualLittle, Is.EqualTo(expected));
+                Assert.That(actualBig, Is.EqualTo(expected));
+            });
+        }
+        else {
+            var actualBig = ToInt64(test_bytes, 0);
             Assert.That(actualBig, Is.EqualTo(expected));
-        });
+        }
     }
 
     [Test]
     public static void TestToUInt64() {
         const ulong expected = 0x0807060504030201;
 
-        IsLittleEndian = true;
-        var actualLittle = ToUInt64(test_bytes, 0);
-        IsLittleEndian = false;
-        var actualBig = ToUInt64(test_bytes, 0);
-        
-        Assert.Multiple(() => {
-            Assert.That(actualLittle, Is.EqualTo(expected));
+        if (BitConverter.IsLittleEndian) {
+            IsLittleEndian = true;
+            var actualLittle = ToUInt64(test_bytes, 0);
+            IsLittleEndian = false;
+            var actualBig = ToUInt64(test_bytes, 0);
+
+            Assert.Multiple(() => {
+                Assert.That(actualLittle, Is.EqualTo(expected));
+                Assert.That(actualBig, Is.EqualTo(expected));
+            });
+        }
+        else {
+            var actualBig = ToUInt64(test_bytes, 0);
             Assert.That(actualBig, Is.EqualTo(expected));
-        });
+        }
     }
 
     [Test]
     public static void TestToSingle() {
         const float expected = 0x04030201;
 
-        IsLittleEndian = true;
-        var actualLittle = ToSingle(test_bytes, 0);
-        IsLittleEndian = false;
-        var actualBig = ToSingle(test_bytes, 0);
-        
-        Assert.Multiple(() => {
-            Assert.That(actualLittle, Is.EqualTo(expected));
+        if (BitConverter.IsLittleEndian) {
+            IsLittleEndian = true;
+            var actualLittle = ToSingle(test_bytes, 0);
+            IsLittleEndian = false;
+            var actualBig = ToSingle(test_bytes, 0);
+
+            Assert.Multiple(() => {
+                Assert.That(actualLittle, Is.EqualTo(expected));
+                Assert.That(actualBig, Is.EqualTo(expected));
+            });
+        }
+        else {
+            var actualBig = ToSingle(test_bytes, 0);
             Assert.That(actualBig, Is.EqualTo(expected));
-        });
+        }
     }
 
     [Test]
     public static void TestToDouble() {
         const double expected = 0x0807060504030201;
 
-        IsLittleEndian = true;
-        var actualLittle = ToDouble(test_bytes, 0);
-        IsLittleEndian = false;
-        var actualBig = ToDouble(test_bytes, 0);
-        
-        Assert.Multiple(() => {
-            Assert.That(actualLittle, Is.EqualTo(expected));
+        if (BitConverter.IsLittleEndian) {
+            IsLittleEndian = true;
+            var actualLittle = ToDouble(test_bytes, 0);
+            IsLittleEndian = false;
+            var actualBig = ToDouble(test_bytes, 0);
+
+            Assert.Multiple(() => {
+                Assert.That(actualLittle, Is.EqualTo(expected));
+                Assert.That(actualBig, Is.EqualTo(expected));
+            });
+        }
+        else {
+            var actualBig = ToDouble(test_bytes, 0);
             Assert.That(actualBig, Is.EqualTo(expected));
-        });
+        }
     }
 }

8f0342f0739d2d323053d18d95cc71bba959dc93
Tomat <xxlennygamerxx@gmail.com>
2023-03-20 20:25:25 -0700

    Stuff.

--- src/GameBreaker.Benchmarks/BenchmarkDotNet.Artifacts/results/GameBreaker.Benchmarks.Serial.BinaryBuffer.Write-report-github.md
+++ src/GameBreaker.Benchmarks/BenchmarkDotNet.Artifacts/results/GameBreaker.Benchmarks.Serial.BinaryBuffer.Write-report-github.md
@@ -4,34 +4,28 @@ BenchmarkDotNet=v0.13.5, OS=Windows 10 (10.0.19045.2728/22H2/2022Update)
 AMD Ryzen 7 5700G with Radeon Graphics, 1 CPU, 16 logical and 8 physical cores
 .NET SDK=7.0.101
   [Host]     : .NET 7.0.4 (7.0.423.11508), X64 RyuJIT AVX2
-  Job-DUZNZJ : .NET 7.0.4 (7.0.423.11508), X64 RyuJIT AVX2
+  Job-VZGRLD : .NET 7.0.4 (7.0.423.11508), X64 RyuJIT AVX2
 
 InvocationCount=1  UnrollFactor=1  
 
 ```
-|                                            Method |     Length |          Mean |       Error |        StdDev |        Median |
-|-------------------------------------------------- |----------- |--------------:|------------:|--------------:|--------------:|
-|                                 **DirectBufferWrite** |        **100** |     **0.0000 ns** |   **0.0000 ns** |     **0.0000 ns** |     **0.0000 ns** |
-|                     BitConverterGetBytesArrayCopy |        100 | 1,102.0833 ns | 106.2014 ns |   306.4153 ns | 1,000.0000 ns |
-|               BitConverterGetBytesBufferBlockCopy |        100 | 1,171.4286 ns |  92.8865 ns |   260.4636 ns | 1,100.0000 ns |
-|       BitConverterTryWriteBytesByteArrayArrayCopy |        100 | 1,488.0435 ns | 149.3925 ns |   421.3642 ns | 1,400.0000 ns |
-| BitConverterTryWriteBytesByteArrayBufferBlockCopy |        100 | 1,126.5957 ns |  93.5389 ns |   266.8718 ns | 1,100.0000 ns |
-|                              UnsafeAsPointerWrite |        100 |    37.8947 ns |  23.3961 ns |    67.1279 ns |     0.0000 ns |
-|                                 **DirectBufferWrite** |     **100000** |    **73.1959 ns** |  **25.6185 ns** |    **74.3240 ns** |   **100.0000 ns** |
-|                     BitConverterGetBytesArrayCopy |     100000 | 1,246.8750 ns | 108.2035 ns |   312.1920 ns | 1,200.0000 ns |
-|               BitConverterGetBytesBufferBlockCopy |     100000 | 1,131.9149 ns | 102.1830 ns |   291.5339 ns | 1,100.0000 ns |
-|       BitConverterTryWriteBytesByteArrayArrayCopy |     100000 | 1,372.0430 ns | 131.2144 ns |   372.2334 ns | 1,400.0000 ns |
-| BitConverterTryWriteBytesByteArrayBufferBlockCopy |     100000 | 1,030.5263 ns |  73.6848 ns |   211.4155 ns | 1,000.0000 ns |
-|                              UnsafeAsPointerWrite |     100000 |    46.8750 ns |  22.4636 ns |    64.8125 ns |     0.0000 ns |
-|                                 **DirectBufferWrite** |  **100000000** |   **720.2128 ns** | **145.5247 ns** |   **415.1900 ns** |   **700.0000 ns** |
-|                     BitConverterGetBytesArrayCopy |  100000000 | 3,955.2083 ns | 419.0576 ns | 1,209.0771 ns | 3,950.0000 ns |
-|               BitConverterGetBytesBufferBlockCopy |  100000000 | 4,253.6842 ns | 410.1817 ns | 1,176.8883 ns | 4,000.0000 ns |
-|       BitConverterTryWriteBytesByteArrayArrayCopy |  100000000 | 4,670.5263 ns | 438.9766 ns | 1,259.5063 ns | 4,700.0000 ns |
-| BitConverterTryWriteBytesByteArrayBufferBlockCopy |  100000000 | 4,587.7778 ns | 491.2156 ns | 1,369.3128 ns | 4,300.0000 ns |
-|                              UnsafeAsPointerWrite |  100000000 |   809.7826 ns | 121.9216 ns |   343.8821 ns |   800.0000 ns |
-|                                 **DirectBufferWrite** | **1000000000** |   **432.9897 ns** | **153.8854 ns** |   **446.4493 ns** |   **300.0000 ns** |
-|                     BitConverterGetBytesArrayCopy | 1000000000 | 5,094.0860 ns | 733.4149 ns | 2,080.5759 ns | 4,850.0000 ns |
-|               BitConverterGetBytesBufferBlockCopy | 1000000000 | 5,105.0562 ns | 592.0394 ns | 1,640.5384 ns | 4,750.0000 ns |
-|       BitConverterTryWriteBytesByteArrayArrayCopy | 1000000000 | 5,175.8065 ns | 489.8794 ns | 1,389.7062 ns | 5,050.0000 ns |
-| BitConverterTryWriteBytesByteArrayBufferBlockCopy | 1000000000 | 4,666.4835 ns | 624.4923 ns | 1,751.1434 ns | 4,550.0000 ns |
-|                              UnsafeAsPointerWrite | 1000000000 |   227.8947 ns | 116.9482 ns |   335.5462 ns |     0.0000 ns |
+|                                            Method |       N |         Mean |       Error |      StdDev |       Median |
+|-------------------------------------------------- |-------- |-------------:|------------:|------------:|-------------:|
+|                                 **DirectBufferWrite** |    **1000** |     **590.5 μs** |    **22.58 μs** |    **66.58 μs** |     **593.1 μs** |
+|                     BitConverterGetBytesArrayCopy |    1000 |     582.9 μs |    28.47 μs |    82.13 μs |     553.1 μs |
+|               BitConverterGetBytesBufferBlockCopy |    1000 |     584.0 μs |    21.88 μs |    64.17 μs |     603.5 μs |
+|       BitConverterTryWriteBytesByteArrayArrayCopy |    1000 |     595.9 μs |    27.58 μs |    79.56 μs |     598.5 μs |
+| BitConverterTryWriteBytesByteArrayBufferBlockCopy |    1000 |     584.5 μs |    25.59 μs |    74.25 μs |     574.2 μs |
+|                              UnsafeAsPointerWrite |    1000 |     588.1 μs |    27.88 μs |    81.77 μs |     575.1 μs |
+|                                 **DirectBufferWrite** |  **100000** |  **17,636.5 μs** |   **343.46 μs** |   **395.53 μs** |  **17,571.4 μs** |
+|                     BitConverterGetBytesArrayCopy |  100000 |  17,637.8 μs |   344.02 μs |   337.88 μs |  17,637.8 μs |
+|               BitConverterGetBytesBufferBlockCopy |  100000 |  17,648.5 μs |   349.84 μs |   429.64 μs |  17,458.5 μs |
+|       BitConverterTryWriteBytesByteArrayArrayCopy |  100000 |  17,795.9 μs |   355.16 μs |   364.72 μs |  17,759.1 μs |
+| BitConverterTryWriteBytesByteArrayBufferBlockCopy |  100000 |  17,559.2 μs |   332.00 μs |   310.55 μs |  17,467.8 μs |
+|                              UnsafeAsPointerWrite |  100000 |  17,993.0 μs |   357.48 μs |   397.34 μs |  18,038.1 μs |
+|                                 **DirectBufferWrite** | **1000000** | **166,733.3 μs** | **3,243.93 μs** | **3,034.37 μs** | **164,873.1 μs** |
+|                     BitConverterGetBytesArrayCopy | 1000000 | 167,020.1 μs | 3,282.32 μs | 3,070.28 μs | 168,336.7 μs |
+|               BitConverterGetBytesBufferBlockCopy | 1000000 | 167,874.5 μs | 3,060.98 μs | 2,863.24 μs | 168,950.5 μs |
+|       BitConverterTryWriteBytesByteArrayArrayCopy | 1000000 | 168,571.1 μs | 2,059.21 μs | 1,926.19 μs | 168,847.6 μs |
+| BitConverterTryWriteBytesByteArrayBufferBlockCopy | 1000000 | 168,572.6 μs | 2,053.39 μs | 1,920.74 μs | 168,898.6 μs |
+|                              UnsafeAsPointerWrite | 1000000 | 168,767.4 μs | 2,823.46 μs | 2,641.07 μs | 169,817.8 μs |
--- src/GameBreaker.Benchmarks/Serial/BinaryBuffer/Write.cs
+++ src/GameBreaker.Benchmarks/Serial/BinaryBuffer/Write.cs
@@ -1,95 +1,183 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Attributes;
+using GameBreaker.Serial;
+using GameBreaker.Serial.Numerics;
 
 namespace GameBreaker.Benchmarks.Serial.BinaryBuffer;
 
 public class Write {
     // ReSharper disable once UnassignedField.Global
-    [Params(100, 100_000, 100_000_000, 1_000_000_000)]
+    [Params(1_000, 100_000, 1_000_000)]
     public int N;
 
-    private int offset;
-    private byte[] buffer = Array.Empty<byte>();
+    private bool b;
+    private byte u8;
+    private short i16;
+    private ushort u16;
+    private Int24 i24;
+    private UInt24 u24;
+    private int i32;
+    private uint u32;
+    private long i64;
+    private ulong u64;
+    private float f32;
+    private double f64;
+    private IBinaryWriter directBufferWriter = null!;
+    private IBinaryWriter getBytesArrayCopyWriter = null!;
+    private IBinaryWriter getBytesBufferBlockCopyWriter = null!;
+    private IBinaryWriter tryWriteBytesArrayCopyWriter = null!;
+    private IBinaryWriter tryWriteBytesBlockCopyWriter = null!;
+    private IBinaryWriter unsafeAsPointerWriter = null!;
 
     [IterationSetup]
     public void Setup() {
-        buffer = new byte[N * sizeof(int)];
-        offset = 0;
+        const int size = sizeof(bool)
+                       + sizeof(byte)
+                       + sizeof(short)
+                       + sizeof(ushort)
+                       + Int24.SIZE
+                       + UInt24.SIZE
+                       + sizeof(int)
+                       + sizeof(uint)
+                       + sizeof(long)
+                       + sizeof(ulong)
+                       + sizeof(float)
+                       + sizeof(double) * 100;
+        directBufferWriter = new BufferBinaryWriter(size);
+        getBytesArrayCopyWriter = new BufferBinaryWriter(size);
+        getBytesBufferBlockCopyWriter = new BufferBinaryWriter(size);
+        tryWriteBytesArrayCopyWriter = new BufferBinaryWriter(size);
+        tryWriteBytesBlockCopyWriter = new BufferBinaryWriter(size);
+        unsafeAsPointerWriter = new BufferBinaryWriter(size);
+
+        var rand = new Random();
+        b = rand.Next(0, 2) == 0;
+        u8 = (byte)rand.Next(byte.MinValue, byte.MaxValue);
+        i16 = (short)rand.Next(short.MinValue, short.MaxValue);
+        u16 = (ushort)rand.Next(ushort.MinValue, ushort.MaxValue);
+        i24 = new Int24(rand.Next(Int24.MinValue, Int24.MaxValue));
+        u24 = new UInt24((uint) rand.Next(0, (int) UInt24.MaxValue));
+        i32 = rand.Next(int.MinValue, int.MaxValue);
+        u32 = (uint)rand.Next(int.MinValue, int.MaxValue);
+        i64 = (long)rand.Next(int.MinValue, int.MaxValue) << 32
+            | (uint)rand.Next(int.MinValue, int.MaxValue);
+        u64 = (ulong)rand.Next(int.MinValue, int.MaxValue) << 32
+            | (uint)rand.Next(int.MinValue, int.MaxValue);
+        f32 = (float)rand.NextDouble();
+        f64 = rand.NextDouble();
     }
 
     [Benchmark]
     public void DirectBufferWrite() {
-        // ReSharper disable once ConvertToConstant.Local
-        var val = 42;
-
         for (var i = 0; i < N; i++) {
-            buffer[offset++] = (byte)(val & 0xFF);
-            buffer[offset++] = (byte)((val >> 8) & 0xFF);
-            buffer[offset++] = (byte)((val >> 16) & 0xFF);
-            buffer[offset++] = (byte)((val >> 24) & 0xFF);
+            directBufferWriter.Write(b, wide: false);
+            directBufferWriter.Write(b, wide: true);
+            directBufferWriter.Write(u8);
+            directBufferWriter.Write(i16);
+            directBufferWriter.Write(u16);
+            directBufferWriter.Write(i24);
+            directBufferWriter.Write(u24);
+            directBufferWriter.Write(i32);
+            directBufferWriter.Write(u32);
+            directBufferWriter.Write(i64);
+            directBufferWriter.Write(u64);
+            directBufferWriter.Write(f32);
+            directBufferWriter.Write(f64);
         }
     }
 
     [Benchmark]
     public void BitConverterGetBytesArrayCopy() {
-        // ReSharper disable once ConvertToConstant.Local
-        var val = 42;
-
         for (var i = 0; i < N; i++) {
-            var bytes = BitConverter.GetBytes(val);
-            Array.Copy(bytes, 0, buffer, offset, bytes.Length);
-            offset += sizeof(int);
+            getBytesArrayCopyWriter.Write(b, wide: false);
+            getBytesArrayCopyWriter.Write(b, wide: true);
+            getBytesArrayCopyWriter.Write(u8);
+            getBytesArrayCopyWriter.Write(i16);
+            getBytesArrayCopyWriter.Write(u16);
+            getBytesArrayCopyWriter.Write(i24);
+            getBytesArrayCopyWriter.Write(u24);
+            getBytesArrayCopyWriter.Write(i32);
+            getBytesArrayCopyWriter.Write(u32);
+            getBytesArrayCopyWriter.Write(i64);
+            getBytesArrayCopyWriter.Write(u64);
+            getBytesArrayCopyWriter.Write(f32);
+            getBytesArrayCopyWriter.Write(f64);
         }
     }
 
     [Benchmark]
     public void BitConverterGetBytesBufferBlockCopy() {
-        // ReSharper disable once ConvertToConstant.Local
-        var val = 42;
-
         for (var i = 0; i < N; i++) {
-            var bytes = BitConverter.GetBytes(val);
-            Buffer.BlockCopy(bytes, 0, buffer, offset, sizeof(int));
-            offset += sizeof(int);
+            getBytesBufferBlockCopyWriter.Write(b, wide: false);
+            getBytesBufferBlockCopyWriter.Write(b, wide: true);
+            getBytesBufferBlockCopyWriter.Write(u8);
+            getBytesBufferBlockCopyWriter.Write(i16);
+            getBytesBufferBlockCopyWriter.Write(u16);
+            getBytesBufferBlockCopyWriter.Write(i24);
+            getBytesBufferBlockCopyWriter.Write(u24);
+            getBytesBufferBlockCopyWriter.Write(i32);
+            getBytesBufferBlockCopyWriter.Write(u32);
+            getBytesBufferBlockCopyWriter.Write(i64);
+            getBytesBufferBlockCopyWriter.Write(u64);
+            getBytesBufferBlockCopyWriter.Write(f32);
+            getBytesBufferBlockCopyWriter.Write(f64);
         }
     }
 
     [Benchmark]
     public void BitConverterTryWriteBytesByteArrayArrayCopy() {
-        // ReSharper disable once ConvertToConstant.Local
-        var val = 42;
-
         for (var i = 0; i < N; i++) {
-            var buf = new byte[sizeof(int)];
-            BitConverter.TryWriteBytes(buf, val);
-            Array.Copy(buf, 0, buffer, offset, buf.Length);
-            offset += sizeof(int);
+            tryWriteBytesArrayCopyWriter.Write(b, wide: false);
+            tryWriteBytesArrayCopyWriter.Write(b, wide: true);
+            tryWriteBytesArrayCopyWriter.Write(u8);
+            tryWriteBytesArrayCopyWriter.Write(i16);
+            tryWriteBytesArrayCopyWriter.Write(u16);
+            tryWriteBytesArrayCopyWriter.Write(i24);
+            tryWriteBytesArrayCopyWriter.Write(u24);
+            tryWriteBytesArrayCopyWriter.Write(i32);
+            tryWriteBytesArrayCopyWriter.Write(u32);
+            tryWriteBytesArrayCopyWriter.Write(i64);
+            tryWriteBytesArrayCopyWriter.Write(u64);
+            tryWriteBytesArrayCopyWriter.Write(f32);
+            tryWriteBytesArrayCopyWriter.Write(f64);
         }
     }
 
     [Benchmark]
     public void BitConverterTryWriteBytesByteArrayBufferBlockCopy() {
-        // ReSharper disable once ConvertToConstant.Local
-        var val = 42;
-
         for (var i = 0; i < N; i++) {
-            var buf = new byte[sizeof(int)];
-            BitConverter.TryWriteBytes(buf, val);
-            Buffer.BlockCopy(buf, 0, buffer, offset, sizeof(int));
-            offset += sizeof(int);
+            tryWriteBytesBlockCopyWriter.Write(b, wide: false);
+            tryWriteBytesBlockCopyWriter.Write(b, wide: true);
+            tryWriteBytesBlockCopyWriter.Write(u8);
+            tryWriteBytesBlockCopyWriter.Write(i16);
+            tryWriteBytesBlockCopyWriter.Write(u16);
+            tryWriteBytesBlockCopyWriter.Write(i24);
+            tryWriteBytesBlockCopyWriter.Write(u24);
+            tryWriteBytesBlockCopyWriter.Write(i32);
+            tryWriteBytesBlockCopyWriter.Write(u32);
+            tryWriteBytesBlockCopyWriter.Write(i64);
+            tryWriteBytesBlockCopyWriter.Write(u64);
+            tryWriteBytesBlockCopyWriter.Write(f32);
+            tryWriteBytesBlockCopyWriter.Write(f64);
         }
     }
 
     [Benchmark]
     public void UnsafeAsPointerWrite() {
-        // ReSharper disable once ConvertToConstant.Local
-        var val = 42;
-
         for (var i = 0; i < N; i++) {
-            ref var b = ref buffer[offset];
-            Unsafe.As<byte, int>(ref b) = val;
-            offset += sizeof(int);
+            unsafeAsPointerWriter.Write(b, wide: false);
+            unsafeAsPointerWriter.Write(b, wide: true);
+            unsafeAsPointerWriter.Write(u8);
+            unsafeAsPointerWriter.Write(i16);
+            unsafeAsPointerWriter.Write(u16);
+            unsafeAsPointerWriter.Write(i24);
+            unsafeAsPointerWriter.Write(u24);
+            unsafeAsPointerWriter.Write(i32);
+            unsafeAsPointerWriter.Write(u32);
+            unsafeAsPointerWriter.Write(i64);
+            unsafeAsPointerWriter.Write(u64);
+            unsafeAsPointerWriter.Write(f32);
+            unsafeAsPointerWriter.Write(f64);
         }
     }
 }

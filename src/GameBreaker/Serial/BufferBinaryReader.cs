385fced221d982c79bd81f4238b8a70331ae8a5b
Tomat <xxlennygamerxx@gmail.com>
2023-03-21 17:33:48 -0700

    File-scoped namespaces for BufferBinaryReader/Writer.

--- src/GameBreaker/Serial/BufferBinaryReader.cs
+++ src/GameBreaker/Serial/BufferBinaryReader.cs
@@ -28,167 +28,167 @@ using System.Text;
 using GameBreaker.Serial.Numerics;
 
 // TODO: Better exceptions.
-namespace GameBreaker.Serial {
-    /// <summary>
-    ///     An implementation of <see cref="IBinaryReader"/> that takes a raw
-    ///     byte array as its backing buffer.
-    /// </summary>
-    public class BufferBinaryReader : IBinaryReader {
+namespace GameBreaker.Serial; 
+
+/// <summary>
+///     An implementation of <see cref="IBinaryReader"/> that takes a raw
+///     byte array as its backing buffer.
+/// </summary>
+public class BufferBinaryReader : IBinaryReader {
 #region IBinaryReader Impl (Properties)
-        private int offset;
+    private int offset;
 
-        /// <inheritdoc cref="IBinaryReader.Offset"/>
-        public int Offset {
-            get => offset;
-            set => offset = value;
-        }
+    /// <inheritdoc cref="IBinaryReader.Offset"/>
+    public int Offset {
+        get => offset;
+        set => offset = value;
+    }
 
-        /// <inheritdoc cref="IBinaryReader.Length"/>
-        public int Length => Buffer.Length;
+    /// <inheritdoc cref="IBinaryReader.Length"/>
+    public int Length => Buffer.Length;
 
-        /// <inheritdoc cref="IBinaryReader.Buffer"/>
-        public byte[] Buffer { get; }
+    /// <inheritdoc cref="IBinaryReader.Buffer"/>
+    public byte[] Buffer { get; }
 
-        /// <inheritdoc cref="IBinaryReader.Encoding"/>
-        public Encoding Encoding { get; }
+    /// <inheritdoc cref="IBinaryReader.Encoding"/>
+    public Encoding Encoding { get; }
 #endregion
 
-        /// <summary>
-        ///     Initializes a new instance of <see cref="BufferBinaryReader"/>.
-        /// </summary>
-        /// <param name="buffer">The backing buffer to use.</param>
-        /// <param name="encoding">The <see cref="Encoding"/> to use.</param>
-        public BufferBinaryReader(byte[] buffer, Encoding? encoding = null) {
-            Buffer = buffer;
-            Encoding = encoding ?? IEncodable.DEFAULT_ENCODING;
-        }
+    /// <summary>
+    ///     Initializes a new instance of <see cref="BufferBinaryReader"/>.
+    /// </summary>
+    /// <param name="buffer">The backing buffer to use.</param>
+    /// <param name="encoding">The <see cref="Encoding"/> to use.</param>
+    public BufferBinaryReader(byte[] buffer, Encoding? encoding = null) {
+        Buffer = buffer;
+        Encoding = encoding ?? IEncodable.DEFAULT_ENCODING;
+    }
 
 #region IBinaryReader Impl (Methods)
-        /// <inheritdoc cref="IBinaryReader.ReadByte"/>
-        public virtual byte ReadByte() {
-            Debug.Assert(Offset >= 0 && Offset + 1 <= Length);
-            return Buffer[Offset++];
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadByte"/>
+    public virtual byte ReadByte() {
+        Debug.Assert(Offset >= 0 && Offset + 1 <= Length);
+        return Buffer[Offset++];
+    }
 
-        /// <inheritdoc cref="IBinaryReader.ReadBoolean"/>
-        public virtual bool ReadBoolean(bool wide) {
-            return GmBitConverter.ToBoolean(Buffer, ref offset, wide: wide);
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadBoolean"/>
+    public virtual bool ReadBoolean(bool wide) {
+        return GmBitConverter.ToBoolean(Buffer, ref offset, wide: wide);
+    }
 
-        /// <inheritdoc cref="IBinaryReader.ReadChars"/>
-        public virtual string ReadChars(int count) {
-            Debug.Assert(Offset >= 0 && Offset + count <= Length);
-            var sb = new StringBuilder();
-            for (int i = 0; i < count; i++)
-                sb.Append(Convert.ToChar(Buffer[Offset++]));
-            return sb.ToString();
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadChars"/>
+    public virtual string ReadChars(int count) {
+        Debug.Assert(Offset >= 0 && Offset + count <= Length);
+        var sb = new StringBuilder();
+        for (int i = 0; i < count; i++)
+            sb.Append(Convert.ToChar(Buffer[Offset++]));
+        return sb.ToString();
+    }
 
-        /// <inheritdoc cref="IBinaryReader.ReadBytes"/>
-        public virtual BufferRegion ReadBytes(int count) {
-            Debug.Assert(Offset >= 0 && Offset + count <= Length);
-            var val = new BufferRegion(Buffer, Offset, count);
-            Offset += count;
-            return val;
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadBytes"/>
+    public virtual BufferRegion ReadBytes(int count) {
+        Debug.Assert(Offset >= 0 && Offset + count <= Length);
+        var val = new BufferRegion(Buffer, Offset, count);
+        Offset += count;
+        return val;
+    }
 
-        /// <inheritdoc cref="IBinaryReader.ReadInt16"/>
-        public virtual short ReadInt16() {
-            Debug.Assert(Offset >= 0 && Offset + 2 <= Length);
-            return GmBitConverter.ToInt16(Buffer, ref offset);
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadInt16"/>
+    public virtual short ReadInt16() {
+        Debug.Assert(Offset >= 0 && Offset + 2 <= Length);
+        return GmBitConverter.ToInt16(Buffer, ref offset);
+    }
 
-        /// <inheritdoc cref="IBinaryReader.ReadUInt16"/>
-        public virtual ushort ReadUInt16() {
-            Debug.Assert(Offset >= 0 && Offset + 2 <= Length);
-            return GmBitConverter.ToUInt16(Buffer, ref offset);
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadUInt16"/>
+    public virtual ushort ReadUInt16() {
+        Debug.Assert(Offset >= 0 && Offset + 2 <= Length);
+        return GmBitConverter.ToUInt16(Buffer, ref offset);
+    }
 
-        /// <inheritdoc cref="IBinaryReader.ReadInt24"/>
-        public virtual Int24 ReadInt24() {
-            Debug.Assert(Offset >= 0 && Offset + 3 <= Length);
-            return GmBitConverter.ToInt24(Buffer, ref offset);
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadInt24"/>
+    public virtual Int24 ReadInt24() {
+        Debug.Assert(Offset >= 0 && Offset + 3 <= Length);
+        return GmBitConverter.ToInt24(Buffer, ref offset);
+    }
 
-        /// <inheritdoc cref="IBinaryReader.ReadUInt24"/>
-        public virtual UInt24 ReadUInt24() {
-            Debug.Assert(Offset >= 0 && Offset + 3 <= Length);
-            return GmBitConverter.ToUInt24(Buffer, ref offset);
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadUInt24"/>
+    public virtual UInt24 ReadUInt24() {
+        Debug.Assert(Offset >= 0 && Offset + 3 <= Length);
+        return GmBitConverter.ToUInt24(Buffer, ref offset);
+    }
 
-        /// <inheritdoc cref="IBinaryReader.ReadInt32"/>
-        public virtual int ReadInt32() {
-            Debug.Assert(Offset >= 0 && Offset + 4 <= Length);
-            return GmBitConverter.ToInt32(Buffer, ref offset);
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadInt32"/>
+    public virtual int ReadInt32() {
+        Debug.Assert(Offset >= 0 && Offset + 4 <= Length);
+        return GmBitConverter.ToInt32(Buffer, ref offset);
+    }
 
-        /// <inheritdoc cref="IBinaryReader.ReadUInt32"/>
-        public virtual uint ReadUInt32() {
-            Debug.Assert(Offset >= 0 && Offset + 4 <= Length);
-            return GmBitConverter.ToUInt32(Buffer, ref offset);
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadUInt32"/>
+    public virtual uint ReadUInt32() {
+        Debug.Assert(Offset >= 0 && Offset + 4 <= Length);
+        return GmBitConverter.ToUInt32(Buffer, ref offset);
+    }
 
-        /// <inheritdoc cref="IBinaryReader.ReadInt64"/>
-        public virtual long ReadInt64() {
-            Debug.Assert(Offset >= 0 && Offset + 8 <= Length);
-            return GmBitConverter.ToInt64(Buffer, ref offset);
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadInt64"/>
+    public virtual long ReadInt64() {
+        Debug.Assert(Offset >= 0 && Offset + 8 <= Length);
+        return GmBitConverter.ToInt64(Buffer, ref offset);
+    }
 
-        /// <inheritdoc cref="IBinaryReader.ReadUInt64"/>
-        public virtual ulong ReadUInt64() {
-            Debug.Assert(Offset >= 0 && Offset + 8 <= Length);
-            return GmBitConverter.ToUInt64(Buffer, ref offset);
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadUInt64"/>
+    public virtual ulong ReadUInt64() {
+        Debug.Assert(Offset >= 0 && Offset + 8 <= Length);
+        return GmBitConverter.ToUInt64(Buffer, ref offset);
+    }
 
-        /// <inheritdoc cref="IBinaryReader.ReadSingle"/>
-        public virtual float ReadSingle() {
-            Debug.Assert(Offset >= 0 && Offset + 4 <= Length);
-            return GmBitConverter.ToSingle(Buffer, ref offset);
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadSingle"/>
+    public virtual float ReadSingle() {
+        Debug.Assert(Offset >= 0 && Offset + 4 <= Length);
+        return GmBitConverter.ToSingle(Buffer, ref offset);
+    }
 
-        /// <inheritdoc cref="IBinaryReader.ReadDouble"/>
-        public virtual double ReadDouble() {
-            Debug.Assert(Offset >= 0 && Offset + 8 <= Length);
-            return GmBitConverter.ToDouble(Buffer, ref offset);
-        }
+    /// <inheritdoc cref="IBinaryReader.ReadDouble"/>
+    public virtual double ReadDouble() {
+        Debug.Assert(Offset >= 0 && Offset + 8 <= Length);
+        return GmBitConverter.ToDouble(Buffer, ref offset);
+    }
 #endregion
 
 #region IDisposable Impl
-        protected virtual void Dispose(bool disposing) {
-            if (disposing) { }
-        }
+    protected virtual void Dispose(bool disposing) {
+        if (disposing) { }
+    }
 
-        public void Dispose() {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+    public void Dispose() {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
 #endregion
 
-        /// <summary>
-        ///     Initializes a <see cref="BufferBinaryReader"/> instance from the
-        ///     given <paramref name="stream"/>. The stream will be closed after
-        ///     reading.
-        /// </summary>
-        /// <param name="stream">The stream to read from.</param>
-        /// <param name="encoding">The encoding to use.</param>
-        /// <returns>The initialized <see cref="BufferBinaryReader"/>.</returns>
-        public static BufferBinaryReader FromStream(
-            Stream stream,
-            Encoding? encoding = null
-        ) {
-            if (stream.Length > int.MaxValue)
-                throw new IOException("Stream is too large");
+    /// <summary>
+    ///     Initializes a <see cref="BufferBinaryReader"/> instance from the
+    ///     given <paramref name="stream"/>. The stream will be closed after
+    ///     reading.
+    /// </summary>
+    /// <param name="stream">The stream to read from.</param>
+    /// <param name="encoding">The encoding to use.</param>
+    /// <returns>The initialized <see cref="BufferBinaryReader"/>.</returns>
+    public static BufferBinaryReader FromStream(
+        Stream stream,
+        Encoding? encoding = null
+    ) {
+        if (stream.Length > int.MaxValue)
+            throw new IOException("Stream is too large");
 
-            var buf = new byte[stream.Length];
+        var buf = new byte[stream.Length];
 
-            stream.Seek(0, SeekOrigin.Begin);
-            var bytes = stream.Read(buf, 0, buf.Length);
-            stream.Close();
+        stream.Seek(0, SeekOrigin.Begin);
+        var bytes = stream.Read(buf, 0, buf.Length);
+        stream.Close();
 
-            if (bytes != buf.Length)
-                throw new IOException("Stream read failed");
+        if (bytes != buf.Length)
+            throw new IOException("Stream read failed");
 
-            return new BufferBinaryReader(buf, encoding);
-        }
+        return new BufferBinaryReader(buf, encoding);
     }
 }
--- src/GameBreaker/Serial/BufferBinaryWriter.cs
+++ src/GameBreaker/Serial/BufferBinaryWriter.cs
@@ -27,216 +27,216 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using GameBreaker.Serial.Numerics;
 
-namespace GameBreaker.Serial {
+namespace GameBreaker.Serial; 
+
+/// <summary>
+///     An implementation of <see cref="IBinaryWriter"/> that uses a raw
+///     byte array as its backing buffer, flushable to a
+///     <see cref="Stream"/>.
+/// </summary>
+public class BufferBinaryWriter : IBinaryWriter {
     /// <summary>
-    ///     An implementation of <see cref="IBinaryWriter"/> that uses a raw
-    ///     byte array as its backing buffer, flushable to a
-    ///     <see cref="Stream"/>.
+    ///     The default size to initialize the buffer.
     /// </summary>
-    public class BufferBinaryWriter : IBinaryWriter {
-        /// <summary>
-        ///     The default size to initialize the buffer.
-        /// </summary>
-        public const int DEFAULT_SIZE = 1024 * 1024 * 32;
+    public const int DEFAULT_SIZE = 1024 * 1024 * 32;
 
-        private byte[] buffer;
-        private int offset;
+    private byte[] buffer;
+    private int offset;
 
-        /// <inheritdoc cref="IBinaryWriter.Offset"/>
-        public int Offset {
-            get => offset;
-            set => EnsureCapacity(offset = value);
-        }
+    /// <inheritdoc cref="IBinaryWriter.Offset"/>
+    public int Offset {
+        get => offset;
+        set => EnsureCapacity(offset = value);
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Length"/>
-        public int Length { get; set; }
+    /// <inheritdoc cref="IBinaryWriter.Length"/>
+    public int Length { get; set; }
 
-        /// <inheritdoc cref="IBinaryWriter.Buffer"/>
-        public byte[] Buffer => buffer;
+    /// <inheritdoc cref="IBinaryWriter.Buffer"/>
+    public byte[] Buffer => buffer;
 
-        /// <inheritdoc cref="IBinaryWriter.Encoding"/>
-        public Encoding Encoding { get; }
+    /// <inheritdoc cref="IBinaryWriter.Encoding"/>
+    public Encoding Encoding { get; }
 
-        /// <summary>
-        ///     Initializes a new instance of <see cref="BufferBinaryWriter"/>.
-        ///     <br />
-        ///     Initializes a new byte array of the specified size.
-        /// </summary>
-        /// <param name="baseSize">
-        ///     The base size to initialize the buffer.
-        /// </param>
-        /// <param name="encoding">The <see cref="Encoding"/> to use.</param>
-        public BufferBinaryWriter(
-            int baseSize = DEFAULT_SIZE,
-            Encoding? encoding = null
-        ) {
-            encoding ??= IEncodable.DEFAULT_ENCODING;
-            buffer = new byte[baseSize];
-            Offset = 0;
+    /// <summary>
+    ///     Initializes a new instance of <see cref="BufferBinaryWriter"/>.
+    ///     <br />
+    ///     Initializes a new byte array of the specified size.
+    /// </summary>
+    /// <param name="baseSize">
+    ///     The base size to initialize the buffer.
+    /// </param>
+    /// <param name="encoding">The <see cref="Encoding"/> to use.</param>
+    public BufferBinaryWriter(
+        int baseSize = DEFAULT_SIZE,
+        Encoding? encoding = null
+    ) {
+        encoding ??= IEncodable.DEFAULT_ENCODING;
+        buffer = new byte[baseSize];
+        Offset = 0;
 
-            Encoding = encoding;
-        }
+        Encoding = encoding;
+    }
 
-        /// <summary>
-        ///     Initializes a new instance of <see cref="BufferBinaryWriter"/>.
-        ///     <br />
-        ///     Uses the specified byte array as the backing buffer.
-        /// </summary>
-        /// <param name="buffer">The buffer this writer should use.</param>
-        /// <param name="encoding">The <see cref="Encoding"/> to use.</param>
-        public BufferBinaryWriter(byte[] buffer, Encoding? encoding = null) {
-            encoding ??= IEncodable.DEFAULT_ENCODING;
-            this.buffer = buffer;
-            Offset = 0;
+    /// <summary>
+    ///     Initializes a new instance of <see cref="BufferBinaryWriter"/>.
+    ///     <br />
+    ///     Uses the specified byte array as the backing buffer.
+    /// </summary>
+    /// <param name="buffer">The buffer this writer should use.</param>
+    /// <param name="encoding">The <see cref="Encoding"/> to use.</param>
+    public BufferBinaryWriter(byte[] buffer, Encoding? encoding = null) {
+        encoding ??= IEncodable.DEFAULT_ENCODING;
+        this.buffer = buffer;
+        Offset = 0;
 
-            Encoding = encoding;
-        }
+        Encoding = encoding;
+    }
 
-        // TODO: Provide option to resize to `size` instead of `Length` * 2?
-        // Mostly for systems with lower memory that may want to minimize
-        // allocations. Detrimental to speed, however. It's a tradeoff.
-        private void EnsureCapacity(int size) {
-            if (Buffer.Length >= size)
-                return;
+    // TODO: Provide option to resize to `size` instead of `Length` * 2?
+    // Mostly for systems with lower memory that may want to minimize
+    // allocations. Detrimental to speed, however. It's a tradeoff.
+    private void EnsureCapacity(int size) {
+        if (Buffer.Length >= size)
+            return;
 
-            var newSize = Math.Max(Buffer.Length * 2, size);
-            Array.Resize(ref buffer, newSize);
-            Length = size;
-        }
+        var newSize = Math.Max(Buffer.Length * 2, size);
+        Array.Resize(ref buffer, newSize);
+        Length = size;
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(byte)"/>
-        public virtual void Write(byte value) {
-            EnsureCapacity(Offset + sizeof(byte));
-            ref var b = ref Buffer[Offset];
-            Unsafe.As<byte, byte>(ref b) = value;
-            Offset += sizeof(byte);
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(byte)"/>
+    public virtual void Write(byte value) {
+        EnsureCapacity(Offset + sizeof(byte));
+        ref var b = ref Buffer[Offset];
+        Unsafe.As<byte, byte>(ref b) = value;
+        Offset += sizeof(byte);
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(bool,bool)"/>
-        public virtual void Write(bool value, bool wide) {
-            if (wide)
-                Write(value ? 1 : 0);
-            else
-                Write((byte) (value ? 1 : 0));
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(bool,bool)"/>
+    public virtual void Write(bool value, bool wide) {
+        if (wide)
+            Write(value ? 1 : 0);
+        else
+            Write((byte) (value ? 1 : 0));
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(BufferRegion)"/>
-        public virtual void Write(BufferRegion value) {
-            EnsureCapacity(Offset + value.Length);
-            value.CopyTo(Buffer.AsMemory().Slice(Offset, value.Length));
-            Offset += value.Length;
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(BufferRegion)"/>
+    public virtual void Write(BufferRegion value) {
+        EnsureCapacity(Offset + value.Length);
+        value.CopyTo(Buffer.AsMemory().Slice(Offset, value.Length));
+        Offset += value.Length;
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(byte[])"/>
-        public virtual void Write(byte[] value) {
-            EnsureCapacity(Offset + value.Length);
-            System.Buffer.BlockCopy(value, 0, Buffer, Offset, value.Length);
-            Offset += value.Length;
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(byte[])"/>
+    public virtual void Write(byte[] value) {
+        EnsureCapacity(Offset + value.Length);
+        System.Buffer.BlockCopy(value, 0, Buffer, Offset, value.Length);
+        Offset += value.Length;
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(char[])"/>
-        public virtual void Write(char[] value) {
-            EnsureCapacity(Offset + value.Length);
+    /// <inheritdoc cref="IBinaryWriter.Write(char[])"/>
+    public virtual void Write(char[] value) {
+        EnsureCapacity(Offset + value.Length);
 
-            // TODO: Specific encoding would be nice. What to do?
-            foreach (var c in value)
-                Buffer[Offset++] = Convert.ToByte(c);
-        }
+        // TODO: Specific encoding would be nice. What to do?
+        foreach (var c in value)
+            Buffer[Offset++] = Convert.ToByte(c);
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(short)"/>
-        public virtual void Write(short value) {
-            EnsureCapacity(Offset + sizeof(short));
-            ref var b = ref Buffer[Offset];
-            Unsafe.As<byte, short>(ref b) = value;
-            Offset += sizeof(short);
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(short)"/>
+    public virtual void Write(short value) {
+        EnsureCapacity(Offset + sizeof(short));
+        ref var b = ref Buffer[Offset];
+        Unsafe.As<byte, short>(ref b) = value;
+        Offset += sizeof(short);
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(ushort)"/>
-        public virtual void Write(ushort value) {
-            EnsureCapacity(Offset + sizeof(ushort));
-            ref var b = ref Buffer[Offset];
-            Unsafe.As<byte, ushort>(ref b) = value;
-            Offset += sizeof(ushort);
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(ushort)"/>
+    public virtual void Write(ushort value) {
+        EnsureCapacity(Offset + sizeof(ushort));
+        ref var b = ref Buffer[Offset];
+        Unsafe.As<byte, ushort>(ref b) = value;
+        Offset += sizeof(ushort);
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(Int24)"/>
-        public virtual void Write(Int24 value) {
-            EnsureCapacity(Offset + Int24.SIZE);
-            ref var b = ref Buffer[Offset];
-            Unsafe.As<byte, Int24>(ref b) = value;
-            Offset += Int24.SIZE;
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(Int24)"/>
+    public virtual void Write(Int24 value) {
+        EnsureCapacity(Offset + Int24.SIZE);
+        ref var b = ref Buffer[Offset];
+        Unsafe.As<byte, Int24>(ref b) = value;
+        Offset += Int24.SIZE;
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(UInt24)"/>
-        public virtual void Write(UInt24 value) {
-            EnsureCapacity(Offset + UInt24.SIZE);
-            ref var b = ref Buffer[Offset];
-            Unsafe.As<byte, UInt24>(ref b) = value;
-            Offset += UInt24.SIZE;
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(UInt24)"/>
+    public virtual void Write(UInt24 value) {
+        EnsureCapacity(Offset + UInt24.SIZE);
+        ref var b = ref Buffer[Offset];
+        Unsafe.As<byte, UInt24>(ref b) = value;
+        Offset += UInt24.SIZE;
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(int)"/>
-        public virtual void Write(int value) {
-            EnsureCapacity(Offset + sizeof(int));
-            ref var b = ref Buffer[Offset];
-            Unsafe.As<byte, int>(ref b) = value;
-            Offset += sizeof(int);
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(int)"/>
+    public virtual void Write(int value) {
+        EnsureCapacity(Offset + sizeof(int));
+        ref var b = ref Buffer[Offset];
+        Unsafe.As<byte, int>(ref b) = value;
+        Offset += sizeof(int);
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(uint)"/>
-        public virtual void Write(uint value) {
-            EnsureCapacity(Offset + sizeof(uint));
-            ref var b = ref Buffer[Offset];
-            Unsafe.As<byte, uint>(ref b) = value;
-            Offset += sizeof(uint);
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(uint)"/>
+    public virtual void Write(uint value) {
+        EnsureCapacity(Offset + sizeof(uint));
+        ref var b = ref Buffer[Offset];
+        Unsafe.As<byte, uint>(ref b) = value;
+        Offset += sizeof(uint);
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(long)"/>
-        public virtual void Write(long value) {
-            EnsureCapacity(Offset + sizeof(long));
-            ref var b = ref Buffer[Offset];
-            Unsafe.As<byte, long>(ref b) = value;
-            Offset += sizeof(long);
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(long)"/>
+    public virtual void Write(long value) {
+        EnsureCapacity(Offset + sizeof(long));
+        ref var b = ref Buffer[Offset];
+        Unsafe.As<byte, long>(ref b) = value;
+        Offset += sizeof(long);
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(ulong)"/>
-        public virtual void Write(ulong value) {
-            EnsureCapacity(Offset + sizeof(ulong));
-            ref var b = ref Buffer[Offset];
-            Unsafe.As<byte, ulong>(ref b) = value;
-            Offset += sizeof(ulong);
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(ulong)"/>
+    public virtual void Write(ulong value) {
+        EnsureCapacity(Offset + sizeof(ulong));
+        ref var b = ref Buffer[Offset];
+        Unsafe.As<byte, ulong>(ref b) = value;
+        Offset += sizeof(ulong);
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(float)"/>
-        public virtual void Write(float value) {
-            EnsureCapacity(Offset + sizeof(float));
-            ref var b = ref Buffer[Offset];
-            Unsafe.As<byte, float>(ref b) = value;
-            Offset += sizeof(float);
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(float)"/>
+    public virtual void Write(float value) {
+        EnsureCapacity(Offset + sizeof(float));
+        ref var b = ref Buffer[Offset];
+        Unsafe.As<byte, float>(ref b) = value;
+        Offset += sizeof(float);
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Write(double)"/>
-        public virtual void Write(double value) {
-            EnsureCapacity(Offset + sizeof(double));
-            ref var b = ref Buffer[Offset];
-            Unsafe.As<byte, double>(ref b) = value;
-            Offset += sizeof(double);
-        }
+    /// <inheritdoc cref="IBinaryWriter.Write(double)"/>
+    public virtual void Write(double value) {
+        EnsureCapacity(Offset + sizeof(double));
+        ref var b = ref Buffer[Offset];
+        Unsafe.As<byte, double>(ref b) = value;
+        Offset += sizeof(double);
+    }
 
-        /// <inheritdoc cref="IBinaryWriter.Flush"/>
-        public virtual void Flush(Stream stream) {
-            stream.Write(Buffer, 0, Length);
-        }
+    /// <inheritdoc cref="IBinaryWriter.Flush"/>
+    public virtual void Flush(Stream stream) {
+        stream.Write(Buffer, 0, Length);
+    }
 
 #region IDisposable Impl
-        protected virtual void Dispose(bool disposing) {
-            if (disposing) { }
-        }
-
-        public void Dispose() {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-#endregion
+    protected virtual void Dispose(bool disposing) {
+        if (disposing) { }
     }
+
+    public void Dispose() {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+#endregion
 }

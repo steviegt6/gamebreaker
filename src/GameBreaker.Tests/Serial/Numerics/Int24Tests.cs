8083665d443663db631c90fb3a3642d4a132cdf9
Tomat <xxlennygamerxx@gmail.com>
2023-05-13 15:28:09 -0700

    Update to .NET 7.0. Also actually commit what I meant to commit last commit.

--- src/GameBreaker.Tests/Serial/Numerics/Int24Tests.cs
+++ src/GameBreaker.Tests/Serial/Numerics/Int24Tests.cs
@@ -1,5 +1,31 @@
-﻿namespace GameBreaker.Tests.Serial.Numerics; 
+﻿using GameBreaker.Serial.Numerics;
 
-public class Int24Tests {
-    
+namespace GameBreaker.Tests.Serial.Numerics;
+
+[TestFixture]
+public static class Int24Tests {
+    [TestCase(int.MinValue, false)]
+    [TestCase(int.MaxValue, false)]
+    [TestCase(Int24.MinValue, true)]
+    [TestCase(Int24.MaxValue, true)]
+    [TestCase(0, true)]
+    [TestCase(1, true)]
+    [TestCase(-1, true)]
+    public static void Int24ConversionEqualityTest(int value, bool equal) {
+        var int24 = new Int24(value);
+        var converted = (int) int24;
+        Assert.That(converted == value, Is.EqualTo(equal));
+    }
+
+    [TestCase(uint.MinValue, true)]
+    [TestCase(uint.MaxValue, false)]
+    [TestCase(UInt24.MinValue, true)]
+    [TestCase(UInt24.MaxValue, true)]
+    [TestCase(0u, true)]
+    [TestCase(1u, true)]
+    public static void UInt24ConversionEqualityTest(uint value, bool equal) {
+        var uint24 = new UInt24(value);
+        var converted = (uint) uint24;
+        Assert.That(converted == value, Is.EqualTo(equal));
+    }
 }
